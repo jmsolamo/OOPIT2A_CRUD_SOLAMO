645c1f4f8395f70fee7f0a35d96a92b0faf6ecdf
ITLAB-16 <ITLAB-16@ITLAB-16>
2023-11-25 13:28:19 +0800

    ss

---
+++ IT2A-Crud-DelaCruz,FrancAlvenn/Form5.Designer.vb
@@ -0,0 +1,54 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form5
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.userIdViewer = New CrystalDecisions.Windows.Forms.CrystalReportViewer()
+        Me.intParam1 = New IT2A_Crud_DelaCruz_FrancAlvenn.IntParam()
+        Me.SuspendLayout()
+        '
+        'userIdViewer
+        '
+        Me.userIdViewer.ActiveViewIndex = -1
+        Me.userIdViewer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.userIdViewer.Cursor = System.Windows.Forms.Cursors.Default
+        Me.userIdViewer.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.userIdViewer.Location = New System.Drawing.Point(0, 0)
+        Me.userIdViewer.Name = "userIdViewer"
+        Me.userIdViewer.Size = New System.Drawing.Size(795, 603)
+        Me.userIdViewer.TabIndex = 0
+        '
+        'Form5
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(795, 603)
+        Me.Controls.Add(Me.userIdViewer)
+        Me.Name = "Form5"
+        Me.Text = "Form5"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents userIdViewer As CrystalDecisions.Windows.Forms.CrystalReportViewer
+    Friend WithEvents intParam1 As IntParam
+End Class
---
+++ IT2A-Crud-DelaCruz,FrancAlvenn/Form5.vb
@@ -0,0 +1,21 @@
+﻿Imports CrystalDecisions.CrystalReports.Engine
+Imports CrystalDecisions.Shared
+Public Class Form5
+
+
+    Private Sub Form5_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim crystalDoc As New ReportDocument
+        crystalDoc.Load("C:\Users\ITLAB-16\source\repos\IT2A-Crud-DelaCruz,FrancAlvenn\IT2A-Crud-DelaCruz,FrancAlvenn\IntParam.rpt")
+        Dim pfDefinition As ParameterFieldDefinition
+        Dim pfDefinitions As ParameterFieldDefinitions
+
+        Dim pVal As New ParameterValues
+        Dim pDVal As New ParameterDiscreteValue
+
+        pDVal.Value = Convert.ToInt32(Form1.txtUID.Text)
+
+        pfDefinitions = crystalDoc.DataDefinition.ParameterFields
+        pfDefinition = pfDefinitions.Item("userid")
+        pVal = pfDefinition.CurrentValues
+    End Sub
+End Class
